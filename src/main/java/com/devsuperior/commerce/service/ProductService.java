c00e93371db402571ad1501a0bd8ea3f3a962fcd
Raul Vitorio <raulvitorio790@gmail.com>
2026-02-13 17:07:28 -0300

    Busca paginada de Produtos

--- src/main/java/com/devsuperior/commerce/service/ProductService.java
+++ src/main/java/com/devsuperior/commerce/service/ProductService.java
@@ -4,9 +4,13 @@ import com.devsuperior.commerce.dto.ProductDTO;
 import com.devsuperior.commerce.entities.Product;
 import com.devsuperior.commerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,5 +25,10 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
