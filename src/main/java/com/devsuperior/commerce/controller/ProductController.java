e0295f821190a17c0a2720d64d39785c13630bae
Raul Vitorio <raulvitorio790@gmail.com>
2026-03-31 21:05:40 -0300

    Consultar catalogo ProductMinDTO

--- src/main/java/com/devsuperior/commerce/controller/ProductController.java
+++ src/main/java/com/devsuperior/commerce/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.devsuperior.commerce.controller;
 
 import com.devsuperior.commerce.dto.ProductDTO;
+import com.devsuperior.commerce.dto.ProductMinDTO;
 import com.devsuperior.commerce.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,8 +28,8 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(@RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
-        Page<ProductDTO> dto = service.findAll(name, pageable);
+    public ResponseEntity<Page<ProductMinDTO>> findAll(@RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
+        Page<ProductMinDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
--- src/main/java/com/devsuperior/commerce/service/ProductService.java
+++ src/main/java/com/devsuperior/commerce/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.commerce.service;
 
 import com.devsuperior.commerce.dto.ProductDTO;
+import com.devsuperior.commerce.dto.ProductMinDTO;
 import com.devsuperior.commerce.entities.Product;
 import com.devsuperior.commerce.repositories.ProductRepository;
 import com.devsuperior.commerce.service.exceptionals.DataBaseException;
@@ -29,9 +30,9 @@ public class ProductService {
         return new ProductDTO(product);
     }
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
         Page<Product> result = repository.searchByName(name, pageable);
-        return result.map(x -> new ProductDTO(x));
+        return result.map(x -> new ProductMinDTO(x));
     }
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
